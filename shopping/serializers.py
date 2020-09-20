3481c5a595852dfdae7fabcd289a95f7d27845ae
alirezaatabaki@icloud.com <alirezaatabaki@icloud.com>
2020-09-20 18:34:56 +0430

    api to show categories and products

--- shopping/serializers.py
+++ shopping/serializers.py
@@ -2,13 +2,25 @@ from rest_framework import serializers
 
 from .models import Category, Product
 
-class CategorySerializer(serializers.ModelSerializer):
+class SubSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['name','id']
+
+class CategorySerializer(serializers.ModelSerializer):
+    sub_category = serializers.SerializerMethodField()
+    def get_sub_category(self,instance):
+        return SubSerializer(instance.subcategory.all(),many=True).data
+
+
+    class Meta:
+        model = Category
+        fields = ['is_sub', 'name', 'sub_category']
+
 
 class ProductListSerializer(serializers.ModelSerializer):
+    category = SubSerializer(many=True)
 
     class Meta:
         model = Product
-        fields = ['name', 'image', 'description', 'price', 'available', 'quantity']
+        fields = ['name', 'image', 'description', 'price', 'available', 'quantity','category']
--- shopping/urls.py
+++ shopping/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
+
 from . import views
+
+from .api_url import router
 
 
 app_name = 'shopping'
@@ -7,4 +10,5 @@ urlpatterns = [
 	path('', views.home, name='home'),
 	re_path(r'category/(?P<slug>[-\w]+)/', views.home, name='category_filter'),
 	re_path(r'detail/(?P<slug>[-\w]+)/', views.product_detail, name='product_detail'),
+	path('api/',include(router.urls))
 ]
